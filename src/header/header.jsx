5c944a61e85b5149c1c6ca652fc384f2a4f5a21a
pranav madhu <pranavmadhu73@gmail.com>
2022-03-12 08:40:14 +0530

    made header responsive and navbar to hamburger menu under 1000px

--- src/header/header.jsx
+++ src/header/header.jsx
@@ -1,6 +1,23 @@
-import "./header.css"
-import logo  from '../images/Bottles Hevane Logo .png'
+import "./header.css";
+import logo  from '../images/Bottles Hevane Logo .png';
+import { AiOutlineMenu } from 'react-icons/ai';
+import { AiOutlineClose } from 'react-icons/ai';
+import { useEffect } from "react";
 export default function Header(){
+    useEffect(() =>{
+        let hamberger = document.getElementById("hamberger-menu");
+        let hambergericon = document.getElementById('hamberger-icon');
+        let hambergercloseicon = document.getElementById('hamberger-close-icon');
+        hambergericon.addEventListener("click", ()=>{
+            hamberger.classList.add("hamburger-nav-bar-active");
+            hambergericon.style.cssText="display:none;"
+        });
+        hambergercloseicon.addEventListener("click", ()=>{
+            hamberger.classList.remove("hamburger-nav-bar-active");
+            hambergericon.style.cssText="display:block;"
+
+        })
+    },[])
     return(
         <header className="header">
             <span className="header-logo-wrapper">
@@ -19,6 +36,17 @@ export default function Header(){
             <ul>
                 <li><a href="#" class="header-links">Admin</a></li>
             </ul>
+            <AiOutlineMenu  className="hamburger-menu-icon" id="hamberger-icon"/>
+            <nav className="hamburger-nav-bar" id="hamberger-menu">
+            <AiOutlineClose  className="close-menu-icon" id="hamberger-close-icon"/>
+                <ul>
+                    <li><a href="#home" class="hamburger-links">Home</a></li>
+                    <li><a href="#" class="hamburger-links">Shop</a></li>
+                    <li><a href="#services" class="hamburger-links">Services</a></li>
+                    <li><a href="#contact" class="hamburger-links">Contact</a></li>
+                    <li><a href="#" class="hamburger-links">Admin</a></li>
+                </ul>
+            </nav>
         </header>
     )
 }
